8a7a33bc026ff33a0a7857ec17e731649736786b
Alexander Knöbel <alex-git@knoebel.info>
2019-11-06 20:06:07 +0100

    add parse-binmap node to support valetudo-RE

---
+++ valetudo-parse-binmap.js
@@ -0,0 +1,26 @@
+const RRMapParser = require("./lib/RRMapParser");
+const zlib = require("zlib");
+
+module.exports = function(RED) {
+    function ValetudoParseBinmapNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        node.on('input', function(msg) {
+            var mapData = msg.payload;
+            new Promise((resolve,reject) => {
+                zlib.gunzip(mapData, (err, data) => {
+                    if (!err) {
+                        resolve(RRMapParser.PARSE(data));
+                    } else {
+                        reject(err);
+                    }
+                });         
+            }).then(parsedMapData => {
+                msg.payload = JSON.stringify(parsedMapData);
+                node.send(msg);
+            }).catch(err => node.error(err, msg));
+        });
+    }
+    RED.nodes.registerType("valetudo-parse-binmap",ValetudoParseBinmapNode);    
+}
