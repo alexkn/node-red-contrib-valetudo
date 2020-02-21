802e6bbdf996f3722dbd1e0d92df3b15f890cef7
Alexander Knöbel <alex-git@knoebel.info>
2020-02-22 00:41:16 +0100

    add test for drawing V0.4.0 FW1886 map

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -1,6 +1,12 @@
 const NodeTestHelper = require("node-red-node-test-helper");
 
 class Helper extends NodeTestHelper.NodeTestHelper {
+    /**
+     * @param {*} testNode
+     * @param {*} testFlow
+     * @param {*} testCredentials
+     * @returns Promise<void>
+     */
     load(testNode, testFlow, testCredentials = {}) {
         return new Promise((resolve, reject) => {
             super.load(testNode, testFlow, testCredentials, (err) => {
@@ -10,6 +16,36 @@ class Helper extends NodeTestHelper.NodeTestHelper {
                     resolve();
                 }
             });
+        });
+    }
+
+    /**
+     * @param {string} id
+     * @returns {import("@node-red/runtime/lib/nodes/Node")}
+     */
+    // @ts-ignore
+    getNode(id) {
+        // @ts-ignore
+        return super.getNode(id);
+    }
+
+    /**
+     * @param {import("@node-red/runtime/lib/nodes/Node")} sendingNode
+     * @param {import("@node-red/runtime/lib/nodes/Node")} ReceivingNode
+     * @returns Promise<any>
+     */
+    createTestPromise(sendingNode, ReceivingNode) {
+        return new Promise((resolve, reject) => {
+            let timeout = setTimeout(() => reject("test timeout"), 2000);
+            ReceivingNode.on("input", function (msg) {
+                clearTimeout(timeout);
+                resolve(msg);
+            });
+            sendingNode.on("call:error", function (err) {
+                clearTimeout(timeout);
+                reject(err);
+            });
+
         });
     }
 }
--- test/valetudo-map-png_spec.js
+++ test/valetudo-map-png_spec.js
@@ -1,4 +1,5 @@
 /// <reference types="@types/mocha" />
+const fs =  require("fs").promises;
 const helper = require("./TestHelper");
 const mapPngNode = require("../nodes/valetudo-map-png");
 
@@ -15,5 +16,24 @@ describe("valetudo-map-png Node", function () {
         var n1 = helper.getNode("n1");
 
         n1.should.have.property("name", "test name");
+    });
+
+    it("should draw JSON string from Valetudo 0.4.0 with Firmware 1886 correctly", async function() {
+        let flow = [
+            { id: "n1", type: "valetudo-map-png", wires:[["n2"]], drawPath: true, drawCharger: true, drawRobot: true },
+            { id: "n2", type: "helper" }
+        ];
+        await helper.load(mapPngNode, flow);
+        let n1 = helper.getNode("n1");
+        let n2 = helper.getNode("n2");
+        let data = await fs.readFile("./test/data/FW1886_0.4.0.json", { encoding: "utf-8" });
+        let expectedPng = await fs.readFile("./test/data/FW1886_0.4.0.png");
+
+        let promise = helper.createTestPromise(n1, n2);
+        n1.receive({ payload: data });
+
+        let msg = await promise;
+
+        msg.payload.should.deepEqual(expectedPng);
     });
 });
