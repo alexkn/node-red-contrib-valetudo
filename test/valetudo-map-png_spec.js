0c2a3db7cc9ac3b3271d989421cf525d6aed632c
Alexander Knöbel <alex-git@knoebel.info>
2020-02-22 10:41:06 +0100

    add tests for RE0.4.0-RE7.1 FW1886

--- test/valetudo-map-png_spec.js
+++ test/valetudo-map-png_spec.js
@@ -12,7 +12,6 @@ describe("valetudo-map-png Node", function () {
     it("should be loaded", async function () {
         var flow = [{ id: "n1", type: "valetudo-map-png", name: "test name" }];
         await helper.load(mapPngNode, flow);
-
         var n1 = helper.getNode("n1");
 
         n1.should.have.property("name", "test name");
@@ -31,7 +30,42 @@ describe("valetudo-map-png Node", function () {
 
         let promise = helper.createTestPromise(n1, n2);
         n1.receive({ payload: data });
+        let msg = await promise;
 
+        msg.payload.should.deepEqual(expectedPng);
+    });
+
+    it("should draw binmap from Valetudo RE 0.4.0-RE7.1 with Firmware 1886 correctly", async function() {
+        let flow = [
+            { id: "n1", type: "valetudo-map-png", wires:[["n2"]], drawPath: true, drawCharger: true, drawRobot: true },
+            { id: "n2", type: "helper" }
+        ];
+        await helper.load(mapPngNode, flow);
+        let n1 = helper.getNode("n1");
+        let n2 = helper.getNode("n2");
+        let data = await fs.readFile("./test/data/FW1886_RE0.4.0-RE7.1.bin");
+        let expectedPng = await fs.readFile("./test/data/FW1886_RE0.4.0-RE7.1.png");
+
+        let promise = helper.createTestPromise(n1, n2);
+        n1.receive({ payload: data });
+        let msg = await promise;
+
+        msg.payload.should.deepEqual(expectedPng);
+    });
+
+    it("should draw JSON string from Valetudo RE 0.4.0-RE7.1 with Firmware 1886 correctly", async function() {
+        let flow = [
+            { id: "n1", type: "valetudo-map-png", wires:[["n2"]], drawPath: true, drawCharger: true, drawRobot: true },
+            { id: "n2", type: "helper" }
+        ];
+        await helper.load(mapPngNode, flow);
+        let n1 = helper.getNode("n1");
+        let n2 = helper.getNode("n2");
+        let data = await fs.readFile("./test/data/FW1886_RE0.4.0-RE7.1.json", { encoding: "utf-8" });
+        let expectedPng = await fs.readFile("./test/data/FW1886_RE0.4.0-RE7.1.png");
+
+        let promise = helper.createTestPromise(n1, n2);
+        n1.receive({ payload: data });
         let msg = await promise;
 
         msg.payload.should.deepEqual(expectedPng);
--- test/valetudo-parse-binmap_spec.js
+++ test/valetudo-parse-binmap_spec.js
@@ -1,6 +1,7 @@
 /// <reference types="@types/mocha" />
-var helper = require("./TestHelper");
-var parseBinNode = require("../nodes/valetudo-parse-binmap");
+const fs =  require("fs").promises;
+const helper = require("./TestHelper");
+const parseBinNode = require("../nodes/valetudo-parse-binmap");
 
 describe("valetudo-parse-binmap Node", function () {
 
@@ -9,11 +10,28 @@ describe("valetudo-parse-binmap Node", function () {
     });
 
     it("should be loaded", async function () {
-        var flow = [{ id: "n1", type: "valetudo-parse-binmap", name: "test name" }];
+        let flow = [{ id: "n1", type: "valetudo-parse-binmap", name: "test name" }];
         await helper.load(parseBinNode, flow);
-
-        var n1 = helper.getNode("n1");
+        let n1 = helper.getNode("n1");
 
         n1.should.have.property("name", "test name");
+    });
+
+    it("should parse binmap from Valetudo RE 0.4.0-RE7.1 with Firmware 1886 correctly", async function() {
+        let flow = [
+            { id: "n1", type: "valetudo-parse-binmap", wires:[["n2"]] },
+            { id: "n2", type: "helper" }
+        ];
+        await helper.load(parseBinNode, flow);
+        let n1 = helper.getNode("n1");
+        let n2 = helper.getNode("n2");
+        let data = await fs.readFile("./test/data/FW1886_RE0.4.0-RE7.1.bin");
+        let expectedJSON = await fs.readFile("./test/data/FW1886_RE0.4.0-RE7.1.json", { encoding: "utf-8" });
+
+        let promise = helper.createTestPromise(n1, n2);
+        n1.receive({ payload: data });
+        let msg = await promise;
+
+        msg.payload.should.deepEqual(JSON.parse(expectedJSON));
     });
 });
