cc03b83cafb4245c30564c2ccc8b5aabd5d97dc6
Alexander Knöbel <alex-git@knoebel.info>
2021-01-10 17:05:15 +0100

    style: enforce eslint:no-trailing-spaces

--- nodes/valetudo-parse-binmap.js
+++ nodes/valetudo-parse-binmap.js
@@ -7,32 +7,32 @@ module.exports = function(RED) {
         var node = this;
 
         node.on("input", (msg, send, done) => {
-            send = send || function() { node.send.apply(node,arguments); }; 
-            done = done || function(err) { 
+            send = send || function() { node.send.apply(node,arguments); };
+            done = done || function(err) {
                 if(err) {
                     node.error(err, msg);
-                }                
+                }
             };
 
-            handleMessage(msg, send, done); 
+            handleMessage(msg, send, done);
         });
 
         async function handleMessage(msg, send, done) {
-            try {     
-                var outputMsg = msg;     
+            try {
+                var outputMsg = msg;
 
                 var mapData = msg.payload;
                 mapData = await Gunzip(mapData);
                 mapData = RRMapParser.PARSE(mapData);
-                
+
                 outputMsg.payload = mapData;
-                send(outputMsg); 
+                send(outputMsg);
                 done();
             } catch (e) {
                 done(e.message);
             }
-          
+
         }
     }
-    RED.nodes.registerType("valetudo-parse-binmap",ValetudoParseBinmapNode);    
+    RED.nodes.registerType("valetudo-parse-binmap",ValetudoParseBinmapNode);
 };
